0e050aa1cc278ef48d2eeeffb6ed96140872cc01
George T <abstractpoint@users.noreply.github.com>
2021-10-08 13:34:36 +0100

    Make tests run correctly locally (#2)

--- test/slpx.test.js
+++ test/slpx.test.js
@@ -13,7 +13,7 @@ const traveler = require("ganache-time-traveler");
 // - Alice wallet can't have any SUSHIx, MATICx
 
 const aliceAddress = "0x3226C9EaC0379F04Ba2b1E1e1fcD52ac26309aeA";
-const ownerAddress = "0xEb22B4EF5f66D52a92c8533361b7BE4576C8C3d3";
+const ownerAddress = "0x1A50a6238eb67285cCD4DF17f75CCe430BAAE2A4";
 const lpTokenAddress = "0x34965ba0ac2451A34a0471F04CCa3F990b8dea27";
 const maticxAddress = "0x3aD736904E9e65189c3000c7DD2c8AC8bB7cD4e3";
 const sushixAddress = "0xDaB943C03f9e84795DC7BF51DdC71DaF0033382b";
